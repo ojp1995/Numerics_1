149eac916d8bd40f2f39913a7b55b5ff19e6afaf
Oliver Phillips <contact.o.j.phillips@gmail.com>
2018-10-22 17:47:04 +0100

    updated code, using skeleton and now has FTCS, FTBS, CTCS(problems) and lax wedroff also with problems

--- anaconda_code/Assignment/FTBS_advection.py
+++ anaconda_code/Assignment/FTBS_advection.py
@@ -9,32 +9,54 @@ Created on Tue Oct  9 15:57:21 2018
 #Attempting to write script for FTBS advection scheme
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import animation
 u = 0.1  #assuming constant speed
 Nx=1000
-
-
-x = np.zeros(Nx)  #creating vector of x with intial value 0 for all entries 
-x_next=np.zeros(Nx)
 dt = 0.001  #time step
-dx = 0.001  #spacial step
-
-x[0] = 1  #intial condition
+dx = 1/(Nx-1)  #spacial step
 
 
-    
-def compute_step (x,dt):
-    x_next=np.zeros(len(x))
+
+def FTBS (x,dt):
+    x_next=np.zeros(Nx)
     x_next[0]=x[0]
-    for i in range (1, len(x)):
+    for i in range (1, Nx):
         x_next[i] = x[i] -u*(dt/dx)*(x[i]-x[i-1])
     return x_next
 
+#x_FTBS = FTBS(x,dt)
 
-for i in range(0,5000):
-    x=compute_step(x,dt)
-    plt.draw()
-    #time.sleep(0.0005)
+#fig=plt.figure()
+#ax=plt.axes( xlim=(0,1), ylim=(0,1))
+#line,=ax.plot([],[],lw=2)
+    
+x = np.zeros(Nx)  #creating vector of x with intial value 0 for all entries 
+for i in range(0,Nx):
+    if (i*dx>0.25 and i*dx<0.75):
+        x[i] = 0.5*(1-np.cos(4*np.pi*(i*dx-0.25)))
+        
 
-plt.plot(np.linspace(0,Nx*dt,Nx),x)
-plt.xlabel('Position x')
 
+for i in range(0,Nx):
+    x_FTBS=FTBS(x,dt)
+    plt.plot(x_FTBS)
+  
+
+#plt.plot(x_FTBS)
+#plt.xlabel('Position x')
+  
+
+###animation  
+#fig=plt.figure()
+#ax=plt.axes( xlim=(0,1), ylim=(0,1))
+#line,=ax.plot([],[],lw=2)
+#
+#def animate (frame):
+#    global x
+#    for i in range(0,500):
+#        x=compute_step(x,dt)
+#        line.set_data(np.linspace(0,1,Nx),x)
+#        return line,
+#    
+#    amim = animation.FuncAnimation(fig,animate,frame=100,interval=int(dt*50000),blit=True)
+#    plt.show()
